5719fc0f28bba10709fc49b1514e9443eb3b4bd8
tomasklima <tomas.klim@gmail.com>
2021-05-07 08:09:57 +0200

    wip

--- packages/suite/src/storage/CHANGELOG.md
+++ packages/suite/src/storage/CHANGELOG.md
@@ -1,17 +1,27 @@
 # Storage changelog
 
+## 23
+
+-   added messageSystem
+
 ## 22
+
 -   added LTC bech32 accounts
 
 ## 21
+
 -   fix tx.amount for btc sent txs (subtract tx.fee from it), add tx.totalSpent field
 
 ## 20
+
 -   format tx.details
+
 ## 19
+
 -   removed keyPath from fiatRates definition
 
 ## 18
+
 -   added device.walletNumber
 
 ## 17
--- packages/suite/src/storage/index.ts
+++ packages/suite/src/storage/index.ts
@@ -3,7 +3,7 @@ import { migrate } from './migrations';
 
 import type { SuiteDBSchema } from './definitions';
 
-const VERSION = 23; // don't forget to add migration and CHANGELOG when changing versions!
+const VERSION = 23; // don't forget to add migration and edit storage CHANGELOG when changing versions!
 
 /**
  *  If the object stores don't already exist then creates them.
